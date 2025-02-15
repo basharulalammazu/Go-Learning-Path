f88e6fa76a9898812c3e25022396245899df8528
Basharul - Alam - Mazu <112147775+basharulalammazu@users.noreply.github.com>
2025-02-15 17:52:10 +0600

    Create 2. Escape Sequences.go

---
+++ 2. Escape Sequences.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("User \n Information")
+    fmt.Println("Name \t ID")
+    // example: "Basharul"
+    fmt.Println("\"Basharul\" \t 22-47903-2")
+}
