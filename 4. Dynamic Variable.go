14f346c9ce82ff49ed84d499a4a84ab090349924
Basharul - Alam - Mazu <112147775+basharulalammazu@users.noreply.github.com>
2025-02-16 21:31:56 +0600

    Create 4. Dynamic Variable.go

---
+++ 4. Dynamic Variable.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {  
+    // dynamic variable declarations
+    var name = "Basharul - Alam - Mazu"
+    var country = "Bangladesh"
+    var age = 24
+    var cgpa = 3.74
+    
+   fmt.Println(name)
+   fmt.Println("Country: ", country)
+   fmt.Println("Age: ", cgpa)
+   fmt.Println("CGPA: ", age);
+  
+}
