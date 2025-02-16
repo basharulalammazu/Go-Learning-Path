2c3c296435718a34d1a9dbc92e9024a3af929286
Basharul - Alam - Mazu <112147775+basharulalammazu@users.noreply.github.com>
2025-02-16 21:37:21 +0600

    Create 5. Shortcut Variable Declaration.go

---
+++ 5. Shortcut Variable Declaration.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+    
+    // shortcut variable declarations
+    name := "Basharul - Alam - Mazu"
+     country := "Bangladesh"
+     age := 24
+     cgpa := 3.74
+    
+   fmt.Println(name)
+   fmt.Println("Country: ", country)
+   fmt.Println("Age: ", cgpa)
+   fmt.Println("CGPA: ", age);
+  
+}
