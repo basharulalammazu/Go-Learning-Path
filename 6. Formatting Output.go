095a34b17cd519b16f289482493e1c357cec139c
Basharul - Alam - Mazu <112147775+basharulalammazu@users.noreply.github.com>
2025-02-17 20:49:58 +0600

    Create 6. Formatting Output.go

---
+++ 6. Formatting Output.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+  name := "Basharul - Alam - Mazu"
+  age := 24
+  country := "Bangladesh"
+  cgpa := 3.74
+  
+  
+  fmt.Printf("%v\n", name)
+  fmt.Printf("%v is %v years old\n", name, age)
+  fmt.Printf("%v originally from %v\n", name, country)
+  fmt.Printf("CGPA: %v", cgpa)
+ }
