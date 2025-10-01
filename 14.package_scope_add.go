c23d8817f435d5c4d88117d61b07329683d9c616
basharulalammazu <basharulalam6@gmail.com>
2025-10-01 20:43:40 +0600

    Add lessons 14: scope examples and package scope demo; fix unused imports

---
+++ 14.package_scope_add.go
@@ -0,0 +1,7 @@
+package main
+
+
+func add(num1 int, num2 int){
+	res := num1 + num2
+	println("Result:", res)
+}
---
+++ 14.package_scope_main.go
@@ -0,0 +1,10 @@
+package main
+
+var (
+	a = 30
+	b = 40
+)
+
+func main() {
+	add(a, b)
+}
