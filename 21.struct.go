81c8338a73fab4857a01b104646612fa934f3ce6
basharulalammazu <basharulalam6@gmail.com>
2025-10-08 21:36:51 +0600

    Add basic struct example (User struct intro)

---
+++ 21.struct.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Name  string // Member veriable or field or property
+	Age int
+}
+
+
+func main() {
+	var user1 User
+	user1 = User{
+		Name: "Basharul",
+		Age: 22,
+	}
+	fmt.Println("User 1:", user1)
+
+
+	user2 := User{ // Instance or object of User type
+		Name: "Mazu",
+		Age: 30,
+	}
+
+	fmt.Println("User 2 Name:", user2.Name)
+	fmt.Println("User 2 Age:", user2.Age)
+}
+
+
+/*
+Struct: A collection of fields
+1. Define a struct type
+2. Create struct instances (objects)
+3. Access and modify struct fields
+
+4. Use struct in functions
+5. Anonymous struct
+6. Nested struct
+7. Struct with methods
+8. Pointer to struct
+9. Struct tags
+10. JSON and struct
+
+
+go run 21.struct.go
+*/
