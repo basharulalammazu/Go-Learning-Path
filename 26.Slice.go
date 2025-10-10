e6f447657f74ff841623be0878b743510df7c628
basharulalammazu <basharulalam6@gmail.com>
2025-10-10 18:24:28 +0600

    Enhance slices lesson: show sorting affects underlying array view

--- 26.Slice.go
+++ 26.Slice.go
@@ -1,6 +1,15 @@
 /*
+	slice => A slice is a dynamically-sized, flexible view into the elements of an array.
+	It is a lightweight data structure that provides access to a contiguous segment of an array.
+	Slices are more commonly used in Go than arrays because they offer greater flexibility and convenience.
 
- */
+	Here are some key points about slices in Go:
+	1. Dynamic Size: Unlike arrays, slices can grow and shrink in size as needed. You can append elements to a slice using the built-in append function.
+	2. Underlying Array: A slice is backed by an underlying array. When you create a slice, it references a portion of an array, and changes made to the slice will affect the underlying array.
+	3. Slice Literal: You can create a slice using a slice literal, which is similar to an array literal but without specifying the size. For example: mySlice := []int{1, 2, 3}.
+	4. Length and Capacity: Slices have both a length (the number of elements in the slice) and a capacity (the maximum number of elements the slice can hold before needing to allocate more memory). You can use the built-in len and cap functions to get these values.
+	5. Slicing Syntax: You can create a new slice from an existing slice or array using slicing syntax. For example: newSlice := mySlice[1:4] creates a new slice containing elements from index 1 to index 3 of mySlice.
+*/
 
 package main
 
@@ -14,9 +23,21 @@ func main(){
 	fmt.Println(arr)
 
 	// Create a slice from the array
-	slices := arr[1:5]
+	slices := arr[1:4] // Pointer 1, length 3, capacity 5
+	fmt.Println("Length of slices:", len(slices))
+	// Length = ending_index - starting_index = 4 - 1 = 3 => "is", "a", "GO"
+	fmt.Println("Capacity of slices:", cap(slices)) // Capacity is calculated from the starting index to the end of the array
+	// Capacity = len(arr) - starting_index = 6 - 1 = 5 => "is", "a", "GO", "Interview", "Question"
 	fmt.Println("Slices:", slices)
 
 	sort.Strings(slices)
 	fmt.Println("Sorted Slices:", slices)
+
+
+	// Create a new slice from the existing slice 
+	slices2 := slices[1:3] // Pointer 1, length 2, capacity 4
+	fmt.Println("Length of slices2:", len(slices2))	
+	fmt.Println("Capacity of slices2:", cap(slices2))
+	fmt.Println("Slices2:", slices2)
+	
 }
