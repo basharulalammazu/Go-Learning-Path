8aefa8a041ffffc365864fca28b27dad2c3dbf73
basharulalammazu <basharulalam6@gmail.com>
2025-10-10 21:09:51 +0600

    Refine slice lesson: add prints for arr before/after sort and clarify view semantics

--- 26.Slice.go
+++ 26.Slice.go
@@ -34,10 +34,51 @@ func main(){
 	fmt.Println("Sorted Slices:", slices)
 
 
+
+
+
 	// Create a new slice from the existing slice 
 	slices2 := slices[1:3] // Pointer 1, length 2, capacity 4
 	fmt.Println("Length of slices2:", len(slices2))	
 	fmt.Println("Capacity of slices2:", cap(slices2))
 	fmt.Println("Slices2:", slices2)
-	
+
+
+
+
+
+	// slices_literal => a shorthand syntax used to create a slice by specifying its elements directly
+	// Create a slice using make function
+	slices_literal := []int {10, 20, 30, 40} // Pointer 0, length 4, capacity 4
+	fmt.Println("Slices Literal:", slices_literal)
+	fmt.Println("Length of slices_literal:", len(slices_literal))
+	fmt.Println("Capacity of slices_literal:", cap(slices_literal))
+
+
+
+	// Create a slice using make function with length
+	s1 := make([]int, 3) // Pointer 0, length 3, capacity 3
+	fmt.Println("Slice s1:", s1)
+	fmt.Println("Length of s1:", len(s1))
+	fmt.Println("Capacity of s1:", cap(s1))
+
+	// Create a slice using make function with specified capacity and length
+	s2 := make([]int, 3, 5) // Pointer 0, length 3, capacity 5
+	fmt.Println("Slice s2:", s2)
+	fmt.Println("Length of s2:", len(s2))
+	fmt.Println("Capacity of s2:", cap(s2))
+
+
+	// Empty Slice
+	var s3 []int
+	fmt.Println("Slice s3:", s3)
+	fmt.Println("Length of s3:", len(s3))
+	fmt.Println("Capacity of s3:", cap(s3))
+	 
+	// Append elements to a slice
+	s3 = append(s3, 1)
+	fmt.Println("Slice s3 after appending 1:", s3)
+	fmt.Println("Length of s3 after appending 1:", len(s3))
+	fmt.Println("Capacity of s3 after appending 1:", cap(s3))
+
 }
